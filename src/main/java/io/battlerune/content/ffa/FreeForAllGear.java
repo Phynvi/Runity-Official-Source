c6dae3a37ba503a04fbf752ff1cbd4156810ef6b
nerikharryl <hamzarsfethi@gmail.com>
2018-06-23 14:17:46 -0700

    Redid parts of the free for all gearing

---
+++ src/main/java/io/battlerune/content/ffa/FreeForAllGear.java
@@ -0,0 +1,35 @@
+package io.battlerune.content.ffa;
+
+import io.battlerune.game.world.items.containers.equipment.EquipmentType;
+import io.battlerune.game.world.items.Item;
+
+/**
+ * Handles the storing of the free for all setups
+ * @author Harryl / Nerik#8690
+ *
+ */
+public enum FreeForAllGear {
+
+	DHAROK(new int[][] { { EquipmentType.WEAPON.getSlot(), 4151, 1 } }, 
+			new Item[] {new Item(1, 1)} ), 
+	
+	HYBRID(new int[][] { { EquipmentType.WEAPON.getSlot(), 4151, 1 } }, 
+			new Item[] {new Item(1, 1)});
+	
+	private int[][] gear;
+	private Item[] inventory;
+
+	FreeForAllGear(int[][] gear, Item[] inventory) {
+		this.gear = gear;
+		this.inventory = inventory;
+	}
+	
+	public int[][] getGear() {
+		return gear;
+	}
+
+	public Item[] getInventory() {
+		return inventory;
+	}
+
+}
