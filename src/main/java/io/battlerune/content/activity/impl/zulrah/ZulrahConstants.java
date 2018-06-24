717b3a4cc7b750e3044c26f28ff348ad8b3a4e49
RSPS Hobo <aminrahman24@gmail.com>
2018-06-24 17:00:03 +0000

    Temp fixed zulrah nulling.

--- src/main/java/io/battlerune/content/activity/impl/zulrah/ZulrahConstants.java
+++ src/main/java/io/battlerune/content/activity/impl/zulrah/ZulrahConstants.java
@@ -6,6 +6,7 @@ import io.battlerune.game.world.position.Position;
  * Holds all the zulrah constants.
  *
  * @author Daniel.
+ * @author Adam Temporarily removed zulrah ID 2043 Due to it causing LOADS of issues.
  */
 public class ZulrahConstants {
 
@@ -16,7 +17,7 @@ public class ZulrahConstants {
     public static final int MAXIMUM_SNAKELINGS = 4;
 
     /** The zulrah identifications. */
-    public static final Integer[] ZULRAH_IDS = new Integer[]{2042, 2043, 2044};
+    public static final Integer[] ZULRAH_IDS = new Integer[]{2042, 2044};
 
     /** All the possible positions zulrah can move to. */
     public static final Position[] LOCATIONS = {new Position(2266, 3072), new Position(2276, 3074), new Position(2273, 3064)};
