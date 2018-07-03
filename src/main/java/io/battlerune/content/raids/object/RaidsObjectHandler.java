a3f7e8280461568362603d4f42c9955c33f8e7b7
hamza <rerded21@gmail.com>
2018-07-03 02:57:49 +0200

    Raids (1) System started full object / npc spawning support

---
+++ src/main/java/io/battlerune/content/raids/object/RaidsObjectHandler.java
@@ -0,0 +1,16 @@
+package io.battlerune.content.raids.object;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.battlerune.content.raids.object.impl.LingTree;
+
+public class RaidsObjectHandler {
+
+	public static Map<RaidsObjectData, RaidsObject> objects = new HashMap<>();
+	
+	public static void load() {
+		
+		objects.put(RaidsObjectData.TREE, new LingTree());
+	}
+}
