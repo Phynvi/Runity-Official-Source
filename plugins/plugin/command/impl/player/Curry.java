d52cfc956fe5593e66fe17744a2f5b9063ab131f
Jordan <Jordan@192.168.1.4>
2018-07-09 19:34:01 +0100

    Curry Command

---
+++ plugins/plugin/command/impl/player/Curry.java
@@ -0,0 +1,21 @@
+package plugin.command.impl.player;
+
+import io.battlerune.content.command.Command;
+import io.battlerune.game.world.entity.mob.player.Player;
+import io.battlerune.net.packet.out.SendMessage;
+
+public class Curry implements Command {
+
+	@Override
+	public void execute(Player player, String[] parts) {
+		// TODO Auto-generated method stub
+		player.send(new SendMessage("Adam fucks curry"));
+	}
+
+	@Override
+	public boolean canUse(Player player) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
