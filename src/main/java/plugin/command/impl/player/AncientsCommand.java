de7c70deb9279ce44ea5bac4b7a3613fefb532c7
Teek <007trey2k@gmail.com>
2018-09-13 01:31:17 +0100

    Thread and ancients command

---
+++ src/main/java/plugin/command/impl/player/AncientsCommand.java
@@ -0,0 +1,25 @@
+package plugin.command.impl.player;
+
+import io.battlerune.Config;
+import io.battlerune.content.command.Command;
+import io.battlerune.content.skill.impl.magic.Spellbook;
+import io.battlerune.game.world.entity.mob.player.Player;
+import io.battlerune.game.world.entity.mob.player.PlayerRight;
+
+public class AncientsCommand implements Command {
+
+	@Override
+	public void execute(Player player, String command, String[] parts) {
+		player.spellbook = Spellbook.ANCIENT;
+		player.interfaceManager.setSidebar(Config.MAGIC_TAB, player.spellbook.getInterfaceId());
+		player.sendMessage("You have switched your magics to ancients..");
+
+	}
+
+	@Override
+	public boolean canUse(Player player) {
+		// TODO Auto-generated method stub
+		return PlayerRight.isDeveloper(player);
+	}
+
+}
---
+++ src/main/java/plugin/command/impl/player/ThreadCommand.java
@@ -0,0 +1,30 @@
+package plugin.command.impl.player;
+
+import io.battlerune.content.command.Command;
+import io.battlerune.game.world.entity.mob.player.Player;
+import io.battlerune.net.packet.out.SendURL;
+
+/**
+ * @author Teek
+ * 
+ * 9/13/2018 <01:28am>
+ */
+public class ThreadCommand implements Command {
+
+	@Override
+	public void execute(Player player, String command, String[] parts) {
+		try {
+			String url = "http://runity.io/forums/index.php?showtopic=" + String.valueOf(parts[0]);
+			player.send(new SendURL(url));
+			player.sendMessage("Opening URL: <col=0000ff>"+url);
+		} catch (Exception e) {
+			player.sendMessage("Failed to execute command.. trying using ::thread id");
+		}
+	}
+
+	@Override
+	public boolean canUse(Player player) {
+		return true;
+	}
+
+}
