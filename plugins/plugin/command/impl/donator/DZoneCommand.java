bd1d568ab2e79e46ac86353eeb3a61bfd5c0aadf
Adam <aminrahman24@gmail.com>
2018-09-29 17:30:37 +0100

    Dzone command shit.

--- plugins/plugin/command/impl/donator/DZoneCommand.java
+++ plugins/plugin/command/impl/donator/DZoneCommand.java
@@ -1,17 +1,19 @@
 package plugin.command.impl.donator;
 
-import io.battlerune.content.activity.impl.allvsone2.AllVsOne2;
+import io.battlerune.Config;
 import io.battlerune.content.command.Command;
+import io.battlerune.content.skill.impl.magic.teleport.Teleportation;
 import io.battlerune.game.world.entity.mob.player.Player;
 import io.battlerune.game.world.entity.mob.player.PlayerRight;
+import io.battlerune.net.packet.out.SendMessage;
 
 public class DZoneCommand implements Command {
 
 	@Override
 	public void execute(Player player, String command, String[] parts) {
-		//Teleportation.teleport(player, Config.DONATOR_ZONE);
-		//player.send(new SendMessage("You have teleported to the Donator Zone!"));
-		AllVsOne2.create(player);
+		Teleportation.teleport(player, Config.DONATOR_ZONE);
+		player.send(new SendMessage("You have teleported to the Donator Zone!"));
+		//AllVsOne2.create(player);
 	}
 
 	@Override
